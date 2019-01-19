8df0e9e4c703c8cafad26f91e05c542c0e6e9aac
vsDeus <44678820+vsDeus@users.noreply.github.com>
2019-01-19 18:30:04 +0200

    Update Overview.py

--- Projects/Overview.py
+++ Projects/Overview.py
@@ -121,23 +121,6 @@
 #
 # paper_doll('Hello')
 
-# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their
-#  sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds
-#  21, return 'BUST
-
-# def blackjack(a, b, c):
-#     if a + b + c <= 21:
-#         return a + b + c
-#     elif a + b + c > 21 and (a == 11 or b == 11 or c == 11):
-#         if (a + b + c) - 10 > 21:
-#             return 'BUST'
-#         else:
-#             return (a + b + c) - 10
-#     else:
-#         return 'BUST'
-#
-# print(blackjack(9, 9, 9))
-
 # SUMMER OF '69: Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and
 # extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
 
